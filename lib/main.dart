594362ea6a9ff7b7151ce6f0592a6990e9675ddc
ascotte <alexandre.scotte@resolving.com>
2020-10-06 16:30:11 +0200

    Add and implements strings centralizing and Internationalizing

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'app_localizations.dart';
 import 'notifiers/AppStateNotifier.dart';
 import 'pages/homepage.dart';
 
 void main() {
   runApp(
     ChangeNotifierProvider<AppStateNotifier>(
-      create: (context) => AppStateNotifier(),
-      child: MyApp(),
-    ),
+        create: (context) => AppStateNotifier(), child: MyApp()),
   );
 }
 
@@ -19,7 +19,36 @@ class MyApp extends StatelessWidget {
     return Consumer<AppStateNotifier>(
       builder: (context, appState, child) {
         return MaterialApp(
-          title: 'Flutter Demo',
+          locale: Locale('fr', 'FR'), // switch between en and ru to see effect
+          // localizationsDelegates: [const DemoLocalizationsDelegate()],
+          // List all of the app's supported locales here
+          supportedLocales: [
+            Locale('en', 'EN'),
+            Locale('fr', 'FR'),
+          ],
+          // These delegates make sure that the localization data for the proper language is loaded
+          localizationsDelegates: [
+            // THIS CLASS WILL BE ADDED LATER
+            // A class which loads the translations from JSON files
+            AppLocalizations.delegate,
+            // Built-in localization of basic text for Material widgets
+            GlobalMaterialLocalizations.delegate,
+            // Built-in localization for text direction LTR/RTL
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          // Returns a locale which will be used by the app
+          localeResolutionCallback: (locale, supportedLocales) {
+            // Check if the current device locale is supported
+            for (var supportedLocale in supportedLocales) {
+              if (supportedLocale.languageCode == locale.languageCode &&
+                  supportedLocale.countryCode == locale.countryCode) {
+                return supportedLocale;
+              }
+            }
+            // If the locale of the device is not supported, use the first one
+            // from the list (English, in this case).
+            return supportedLocales.first;
+          },
           theme: ThemeData.light().copyWith(
             accentColor: Colors.blue[900],
             iconTheme: IconThemeData(color: Colors.black),
@@ -29,7 +58,7 @@ class MyApp extends StatelessWidget {
             iconTheme: IconThemeData(color: Colors.white),
           ),
           themeMode: appState.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-          home: MyHomePage(title: 'Flutter Demo Home Page'),
+          home: MyHomePage(),
         );
       },
     );
