6b331692d505a88047fb5d00f3ffc99190f60844
ascotte <alexandre.scotte@resolving.com>
2020-10-14 17:02:37 +0200

    - Add model classes

---
+++ lib/model/profile.dart
@@ -0,0 +1,44 @@
+import 'hobby.dart';
+
+class Profile {
+  int age;
+  String backgroundImageUrl;
+  String birthDate;
+  String description;
+  String firstName;
+  List<Hobby> hobbies;
+  String imageUrl;
+  String job;
+  String lastName;
+  String location;
+
+  Profile(
+      {this.age,
+      this.backgroundImageUrl,
+      this.birthDate,
+      this.description,
+      this.firstName,
+      this.hobbies,
+      this.imageUrl,
+      this.job,
+      this.lastName,
+      this.location});
+
+  Profile.fromJson(Map<String, dynamic> json) {
+    age = json['age'];
+    backgroundImageUrl = json['backgroundImageUrl'];
+    birthDate = json['birthDate'];
+    description = json['description'];
+    firstName = json['firstName'];
+    if (json['hobbies'] != null) {
+      hobbies = new List<Hobby>();
+      json['hobbies'].forEach((v) {
+        hobbies.add(new Hobby.fromJson(v));
+      });
+    }
+    imageUrl = json['imageUrl'];
+    job = json['job'];
+    lastName = json['lastName'];
+    location = json['location'];
+  }
+}
