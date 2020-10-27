bb3807f6c28c10ac89888cd62c8488cd57b04e7a
ascotte <alexandre.scotte@resolving.com>
2020-10-27 13:55:31 +0100

    - Update profile UI with skills chip list

--- lib/pages/navigation_pages/profile_page.dart
+++ lib/pages/navigation_pages/profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:cvflutter/bloc/profile_bloc.dart';
 import 'package:cvflutter/model/profile.dart';
-import 'package:cvflutter/widgets/horizontal_alignment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -26,8 +25,12 @@ class _ProfilePageState extends State<ProfilePage> {
             title: StreamBuilder<Profile>(
                 stream: profileBloc.profile,
                 builder: (context, snapshot) {
-                  return new Text(
-                      "${snapshot.data.firstName} ${snapshot.data.lastName}");
+                  if (snapshot == null || snapshot.data == null) {
+                    return new Text("");
+                  } else {
+                    return new Text(
+                        "${snapshot.data.firstName} ${snapshot.data.lastName}");
+                  }
                 })),
         body: new Center(
           child: StreamBuilder(
@@ -43,26 +46,33 @@ class _ProfilePageState extends State<ProfilePage> {
         ));
   }
 
-  Container buildProfileScreen(Profile data) {
-    return Container(
+  SingleChildScrollView buildProfileScreen(Profile data) {
+    return SingleChildScrollView(
       padding: const EdgeInsets.all(10.0),
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             buildImageHeader(data),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
             new Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[new Text(data.job)]),
+            Padding(padding: EdgeInsets.only(top: 10.0)),
             buildLocationRow(data),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
             new Text(AppLocalizations.of(context).translate('me_profile'),
                 style: Theme.of(context).textTheme.title),
+            Padding(padding: EdgeInsets.only(top: 5.0)),
             new Text(data.description),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
             new Text(AppLocalizations.of(context).translate('me_skills'),
                 style: Theme.of(context).textTheme.title),
+            buildSkillsChips(data),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
             new Text(AppLocalizations.of(context).translate('me_hobbies'),
                 style: Theme.of(context).textTheme.title),
-            buildChips(data),
+            buildHobbiesChips(data),
           ]),
     );
   }
@@ -101,21 +111,34 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  Widget buildChips(Profile data) {
+  Widget buildSkillsChips(Profile data) {
+    if (data != null || data.distinctSkills != null) {
+      return this.buildChips(data.distinctSkills.map((sk) => sk.name).toList());
+    } else {
+      return null;
+    }
+  }
+
+  Widget buildHobbiesChips(Profile data) {
+    if (data != null || data.hobbies != null) {
+      return this.buildChips(data.hobbies.map((sk) => sk.name).toList());
+    } else {
+      return null;
+    }
+  }
+
+  Widget buildChips(List<String> items) {
     List<Widget> chips = new List<Widget>();
 
-    if (data != null || data.hobbies != null) {
-      for (int i = 0; i < data.hobbies.length; i++) {
-        var hobby = data.hobbies[i];
-        ChoiceChip choiceChip = ChoiceChip(
-          selected: false,
-          label: Text(hobby.name),
-          shadowColor: Colors.transparent,
-          backgroundColor: Colors.black54,
-        );
+    for (int i = 0; i < items.length; i++) {
+      var item = items[i];
+      ChoiceChip choiceChip = ChoiceChip(
+        selected: false,
+        label: Text(item),
+        shadowColor: Colors.transparent,
+      );
 
-        chips.add(choiceChip);
-      }
+      chips.add(choiceChip);
     }
 
     return Column(
