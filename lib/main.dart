9ddca2d92afe6da1dd99e439bcd60862d031b3c8
ascotte <alexandre.scotte@resolving.com>
2020-11-05 11:19:34 +0100

    - delete comments

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,23 @@ class MyApp extends StatelessWidget {
     return Consumer<AppStateNotifier>(
       builder: (context, appState, child) {
         return MaterialApp(
-          locale: Locale('fr', 'FR'), // switch between en and ru to see effect
-          // localizationsDelegates: [const DemoLocalizationsDelegate()],
-          // List all of the app's supported locales here
+          locale: Locale('fr', 'FR'),
           supportedLocales: [
             Locale('en', 'EN'),
             Locale('fr', 'FR'),
           ],
-          // These delegates make sure that the localization data for the proper language is loaded
           localizationsDelegates: [
-            // THIS CLASS WILL BE ADDED LATER
-            // A class which loads the translations from JSON files
             AppLocalizations.delegate,
-            // Built-in localization of basic text for Material widgets
             GlobalMaterialLocalizations.delegate,
-            // Built-in localization for text direction LTR/RTL
             GlobalWidgetsLocalizations.delegate,
           ],
-          // Returns a locale which will be used by the app
           localeResolutionCallback: (locale, supportedLocales) {
-            // Check if the current device locale is supported
             for (var supportedLocale in supportedLocales) {
               if (supportedLocale.languageCode == locale.languageCode &&
                   supportedLocale.countryCode == locale.countryCode) {
                 return supportedLocale;
               }
             }
-            // If the locale of the device is not supported, use the first one
-            // from the list (English, in this case).
             return supportedLocales.first;
           },
           theme: ThemeData.light().copyWith(
@@ -63,7 +52,6 @@ class MyApp extends StatelessWidget {
                 body1: TextStyle(fontSize: 14, color: Colors.black),
                 body2: TextStyle(fontSize: 12, color: Colors.black),
               )),
-
           darkTheme: ThemeData.dark().copyWith(
               accentColor: Colors.cyanAccent[400],
               iconTheme: IconThemeData(color: Colors.white),
